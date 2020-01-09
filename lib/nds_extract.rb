5a2cac5a6302a49b20797cc6f874b9e86cdc448e
Jeremy Garnes <j06garnes@gmail.com>
2020-01-09 01:02:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,11 @@ require 'pry'
 def gross_for_director(director_data)
   new_hash = {}
    row_index = 0
- while row_index < nds.length do
-    new_hash[(nds[row_index][:name])]= 0
+ while row_index < director_data.length do
+    new_hash[(director_data[row_index][:name])]= 0
     column_index = 0
-  while column_index < nds[row_index][:movies].length do
-    new_hash[(nds[row_index][:name])] += nds[row_index][:movies][column_index][:worldwide_gross]
+  while column_index < director_data[row_index][:movies].length do
+    new_hash[(director_data[row_index][:name])] += director_data[row_index][:movies][column_index][:worldwide_gross]
      column_index += 1
     end
      row_index += 1
